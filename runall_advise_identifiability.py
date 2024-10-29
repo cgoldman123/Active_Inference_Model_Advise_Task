4e61b2d6f13ec0b8db8442c78865871434edf7ee
hsuBnOediH <li.8906@buckeyemail.osu.edu>
2024-10-29 16:56:47 -0700

    Update runall_advise_identifiability.py

--- runall_advise_identifiability.py
+++ runall_advise_identifiability.py
@@ -20,11 +20,9 @@ with open(subject_list_path) as infile:
 ssub_path = '/mnt/dell_storage/labs/rsmith/lab-members/fli/advise_task/Active-Inference-Model-for-Advise-Task/run_advise_identifiability.ssub'
 
 for idx_candidate in [1,2,3,4,5,6,7,8,9,10]:
-# i = 0
     for subject in subjects:
         stdout_name = f"{results}/logs/{subject}-%J.stdout"
         stderr_name = f"{results}/logs/{subject}-%J.stderr"
-
         jobname = f'advise-fit-{subject}'
         os.system(f"sbatch -J {jobname} -o {stdout_name} -e {stderr_name} {ssub_path} {subject} {results} {idx_candidate}")
 
